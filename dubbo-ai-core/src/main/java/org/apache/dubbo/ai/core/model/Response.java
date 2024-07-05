d290b1bd0b1ee63f7ff8c2226fd169647e68c0d2
liuzhifei <2679431923@qq.com>
2024-07-05 14:11:38 +0800

    init springboot starter

--- dubbo-ai-core/src/main/java/org/apache/dubbo/ai/core/model/Response.java
+++ dubbo-ai-core/src/main/java/org/apache/dubbo/ai/core/model/Response.java
@@ -16,10 +16,6 @@
  */
 package org.apache.dubbo.ai.core.model;
 
-/**
- * @author liuzhifei
- * @since 1.0
- */
 public interface Response<T> {
     
     T getContent();
--- dubbo-ai-openai/src/test/java/org/apache/dubbo/ai/openai/MyAiService.java
+++ dubbo-ai-openai/src/test/java/org/apache/dubbo/ai/openai/MyAiService.java
@@ -1,13 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dubbo.ai.openai;
 
 import org.apache.dubbo.ai.core.DubboAiService;
 import org.apache.dubbo.ai.core.Prompt;
 import org.apache.dubbo.common.stream.StreamObserver;
 
-/**
- * @author liuzhifei
- * @since 1.0
- */
+
 @DubboAiService(configPath = "dubbo-ai.properties", providerConfigs = {"m1", "m2"},model = "gpt-4-all")
 public interface MyAiService {
 
