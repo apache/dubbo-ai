5764bbce12af0230ba5ecc3dcfe457f70c1e7aa8
liuzhifei <2679431923@qq.com>
2024-08-21 19:01:41 +0800

    unified dubbo service export logic

--- java/dubbo-ai-core/src/main/java/org/apache/dubbo/ai/core/DubboAiServiceExporter.java
+++ java/dubbo-ai-core/src/main/java/org/apache/dubbo/ai/core/DubboAiServiceExporter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.dubbo.ai.spring.boot;
+package org.apache.dubbo.ai.core;
 
 import org.apache.dubbo.common.threadpool.manager.FrameworkExecutorRepository;
 import org.apache.dubbo.config.ApplicationConfig;
